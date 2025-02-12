79f419419df1480359af42374acf0ce77f6e4df9
miko0o0o <meirzhan2003@bk.ru>
2025-02-12 20:07:14 +0100

    db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'university_timetable'; // Название твоей базы данных
-$username = 'root'; // Логин MySQL (обычно 'root' в XAMPP)
-$password = ''; // Оставь пустым, если у тебя нет пароля
+$dbname = 'university_timetable'; 
+$username = 'root'; 
+$password = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
